c49ac45b62eabe9b025cbb8ed0d1d787196f8b17
srinivasan-M <srinivasanksr15@gmail.com>
2017-04-17 21:37:08 +0530

    Update even.java

--- even.java
+++ even.java
@@ -5,6 +5,7 @@ public static void main(String args[])
 {
 int sl,el,i;
 Scanner a=new Scanner(System.in);
+ //enter the starting and ending limit 
 sl=a.nextInt();
 el=a.nextInt();
 for(i=sl;i<=el;i++)
